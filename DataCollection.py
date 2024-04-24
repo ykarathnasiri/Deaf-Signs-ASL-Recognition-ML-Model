60786f918ad44a8c5cc1dbc09a395df1a3b96e2d
YKA Rathnasiri <kevinanjaloyr@gmail.com>
2024-04-24 12:26:06 +0530

    Tracking the Hands and crop the image

---
+++ DataCollection.py
@@ -0,0 +1,28 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+
+offset = 20
+imgSize = 300
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands(0)
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
+        imgCrop = img[y-offset:y +h+offset, x-offset:x+w+offset]
+
+        imgCropShape = imgCrop.shape
+
+        imgWhite[0:imgCropShape[0], 0:imgCropShape[1]] = imgCrop
+
+        cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
