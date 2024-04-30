a3e64f58f279e7f81650a49a7fe489942367df88
YKA Rathnasiri <kevinanjaloyr@gmail.com>
2024-04-30 20:51:30 +0530

    implemet to put both hands in white frame

--- DataCollection.py
+++ DataCollection.py
@@ -3,54 +3,48 @@ from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 import math
 import time
+import os
+
+def draw_hand(imgCrop, imgWhite, imgSize, offset=(0,0)):
+    imgCropShape =imgCrop.shape
+    aspectRatio = imgCropShape [0] / imgCropShape[1]
+
+    if aspectRatio > 1:
+            k = imgSize / imgCropShape[0]
+            wCal = math.ceil(k * imgCropShape[1])
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite[:, wGap + offset[1]: wCal + wGap + offset[1]] = imgResize
+
+        else:
+            k = imgSize / imgCropShape[1]
+            hCal = math.ceil(k * imgCropShape[0])
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap + offset[0]:hCal + hGap + offset[0], :] = imgResize
+
+        return imgWhite
+
 
 cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands=1)
+detector = HandDetector(maxHands=2) #detect up to 2 hands
 
 offset = 20
 imgSize = 300
 
 folder = "Data/A"
 counter = 0
+
 while True:
     success, img = cap.read()
     hands, img = detector.findHands(img)
+
     if hands:
-        hand = hands[0]
-        x, y, w, h = hand['bbox']
-
-        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
-        imgCrop = img[y-offset:y +h+offset, x-offset:x+w+offset]
-
-        imgCropShape = imgCrop.shape
-
-
-
-        aspectRatio = h/w
-
-        if aspectRatio > 1:
-            k = imgSize/h
-            wCal = math.ceil(k*w)
-            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
-            imgResizeShape = imgResize.shape
-            wGap = math.ceil((300-wCal)/2)
-            imgWhite[:, wGap: wCal + wGap ] = imgResize
-
-        else:
-            k = imgSize / w
-            hCal = math.ceil(k * h)
-            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
-            imgResizeShape = imgResize.shape
-            hGap = math.ceil((300 - hCal) / 2)
-            imgWhite[hGap: hCal + hGap,:] = imgResize
-
-
-
-        cv2.imshow("ImageCrop", imgCrop)
-        cv2.imshow("ImageWhite", imgWhite)
+        imgWhite
 
     cv2.imshow("Image", img)
     key = cv2.waitKey(1)
     if key == ord("s"):
         counter += 1
-        cv2.imwrite(f'{folder}/Image_{time.time().jpg}',imgWhite)
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
+        print(counter)
