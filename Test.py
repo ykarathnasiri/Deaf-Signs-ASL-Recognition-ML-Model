6c3344aca15c81b5ace513d9305d74a14207ae0b
YKA Rathnasiri <kevinanjaloyr@gmail.com>
2024-05-01 11:24:34 +0530

    Test model

--- Test.py
+++ Test.py
@@ -1,7 +1,9 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
 import numpy as np
 import math
+
 
 def draw_hand(imgCrop, imgWhite, imgSize, offset=(0, 0)):
     imgCropShape = imgCrop.shape
@@ -13,6 +15,8 @@ def draw_hand(imgCrop, imgWhite, imgSize, offset=(0, 0)):
         imgResize = cv2.resize(imgCrop, (wCal, imgSize))
         wGap = math.ceil((imgSize - wCal) / 2)
         imgWhite[:, wGap + offset[1]:wCal + wGap + offset[1]] = imgResize
+        prediction, index = classifier.getPrediction(img)
+        print(prediction, index)
 
     else:
         k = imgSize / imgCropShape[1]
@@ -26,12 +30,15 @@ def draw_hand(imgCrop, imgWhite, imgSize, offset=(0, 0)):
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=2)  # Detect up to 2 hands
+classifier = Classifier("Model\keras_model.h5", "Model\labels.txt")
 
 offset = 20
 imgSize = 300
 
 folder = "Data/A"
 counter = 0
+
+labels = ["A", "B", "C"]
 
 while True:
     success, img = cap.read()
@@ -66,4 +73,3 @@ while True:
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
-
--- test2.py
+++ test2.py
@@ -4,16 +4,16 @@ import numpy as np
 import math
 import time
 
-def draw_hand(imgCrop, imgWhite, imgSize, offset=(0, 0)):
+def draw_hand(imgCrop, imgWhite, imgSize, offset=(0,0)):
     imgCropShape = imgCrop.shape
-    aspectRatio = imgCropShape[0] / imgCropShape[1]
+    aspectRatio = imgCropShape [0] / imgCropShape[1]
 
     if aspectRatio > 1:
-        k = imgSize / imgCropShape[0]
-        wCal = math.ceil(k * imgCropShape[1])
-        imgResize = cv2.resize(imgCrop, (wCal, imgSize))
-        wGap = math.ceil((imgSize - wCal) / 2)
-        imgWhite[:, wGap + offset[1]:wCal + wGap + offset[1]] = imgResize
+            k = imgSize / imgCropShape[0]
+            wCal = math.ceil(k * imgCropShape[1])
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite[:, wGap + offset[1]:wCal + wGap + offset[1]] = imgResize
 
     else:
         k = imgSize / imgCropShape[1]
@@ -24,9 +24,8 @@ def draw_hand(imgCrop, imgWhite, imgSize, offset=(0, 0)):
 
     return imgWhite
 
-
 cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands=2)  # Detect up to 2 hands
+detector = HandDetector(maxHands=2) #detect up to 2 hands
 
 offset = 20
 imgSize = 300
@@ -39,26 +38,26 @@ while True:
     hands, img = detector.findHands(img)
 
     if hands:
-        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255  # Create a white background
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255  #create a white frame
 
-        if len(hands) == 2:  # If both hands are detected
+        if len(hands) == 2: #if both hands are detect
             hand1 = hands[0]
             hand2 = hands[1]
             x1, y1, w1, h1 = hand1['bbox']
             x2, y2, w2, h2 = hand2['bbox']
 
-            # Combine the bounding boxes of both hands
+            #combine the boxes of both hands
             x = min(x1, x2)
             y = min(y1, y2)
             w = max(x1 + w1, x2 + w2) - x
             h = max(y1 + h1, y2 + h2) - y
 
-            # Crop the frame with both hands
+            #crop frame with both hands
             imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
             imgWhite = draw_hand(imgCrop, imgWhite, imgSize)
             cv2.imshow("ImageWhite", imgWhite)
 
-        elif len(hands) == 1:  # If one hand is detected
+        elif len(hands) == 1: #if one hand is detected
             hand = hands[0]
             x, y, w, h = hand['bbox']
             imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
@@ -70,8 +69,4 @@ while True:
     if key == ord("s"):
         counter += 1
         cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
-    elif key == ord("s"):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+        print(counter)
